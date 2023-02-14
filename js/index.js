f1a6b55631c2b57454e91e464049551a14dd2028
eunbinnie <dmsqlsdl54@naver.com>
2023-02-14 19:09:18 +0900

    내 내용 1차 수정

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // 마우스 한 번 아래로 드래그한 위치부터 opacity 60%로 설정
+const navForm = document.querySelector("nav-icon")
+const search = document.getElementById("search-icon");
+const searchInput = document.getElementById("search-input");
+
+console.dir(search)
+
+function searchClick() {
+  searchInput.classList.remove('hidden')
+}
+
+search.addEventListener('click', searchClick);
