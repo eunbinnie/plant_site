d96510773c476a5579601d911d9d3e3b2972da4f
eunbinnie <dmsqlsdl54@naver.com>
2023-03-07 23:38:34 +0900

    header layout, opacity 수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const upIcon = document.getElementById("up-icon");
 const hiddenUpIcon = document.querySelector("#hidden-up-icon");
+const header = document.querySelector('header');
 
 upIcon.addEventListener('click', function () {
   window.scrollTo({
@@ -10,15 +11,17 @@ upIcon.addEventListener('click', function () {
 
 window.addEventListener('scroll', function () {
   if (scrollY > 580) {
+    header.style.opacity = 0.7;
+    header.style.transition = `opacity 0.3s ease-in-out`;
+
     hiddenUpIcon.classList.remove("hidden")
     gsap.to(upIcon, .6, {
       x: 0
     });
   } else {
+    header.style.opacity = 1;
     gsap.to(upIcon, .6, {
       x: 100
     });
   }
 }, 300)
-
-console.log(hiddenUpIcon)
